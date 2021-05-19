58d1e29f548ac0fe0d21e909c1cce2071bb178c3
RaulQX <raul.sarbu@student.upt.ro>
2021-05-20 00:51:58 +0300

    Added basic controllers

---
+++ src/main/java/controller/login/LoginController.java
@@ -0,0 +1,27 @@
+package controller.login;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameTextField;
+    private PasswordField passwordPasswordField;
+
+    private Label usernameErrLabel;
+    private Label passErrLabel;
+
+    public void signInClick() {
+
+    }
+
+    public void createAccountClick() {
+
+    }
+
+}
+
